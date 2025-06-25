a77869285cbf51b7fac8dae44475a17d72e057b4
Matheus Barzon <matheusbarzon@gmail.com>
2025-06-25 16:20:21 -0300

    feat: add parameter in, out and in out

--- nodes/Oracle/OracleDatabase.node.ts
+++ nodes/Oracle/OracleDatabase.node.ts
@@ -96,6 +96,19 @@ export class OracleDatabase implements INodeType {
                   { name: 'No', value: false },
                   { name: 'Yes', value: true }
                 ]
+              },
+              {
+                displayName: 'Parameter Direction',
+                name: 'direction',
+                type: 'options',
+                required: true,
+                default: 'in',
+                hint: 'Specifies if the parameter is for input (IN), output (OUT), or both (IN/OUT).',
+                options: [
+                  { name: 'In', value: 'in' },
+                  { name: 'Out', value: 'out' },
+                  { name: 'In/Out', value: 'inout' }
+                ],
               }
             ],
           },
@@ -130,12 +143,23 @@ export class OracleDatabase implements INodeType {
       //get query
       let query = this.getNodeParameter("query", 0) as string;
 
+      const defaultExecuteManyOptions = {
+        outFormat: oracledb.OUT_FORMAT_OBJECT,
+        autoCommit: true,
+      };
+
+      const executeManyOptions = {
+        bindDefs: [
+          { ...defaultExecuteManyOptions, type: oracledb.NUMBER },
+        ],
+      };
+
+      const bindParameters: oracledb.BindParameters[] = [];
+
       //get list of param objects entered by user:
       const parameterIDataObjectList = ((this.getNodeParameter('params', 0, {}) as IDataObject).values as { name: string, value: string | number, datatype: string, parseInStatement: boolean }[]) || [];
-      
-      //convert parameterIDataObjectList to map of BindParameters that OracleDB wants
-      const bindParameters: { [key: string]: oracledb.BindParameter } = parameterIDataObjectList.reduce((result: { [key: string]: oracledb.BindParameter }, item) => {
 
+      for (const item of parameterIDataObjectList) {
         //set data type to be correct type
         let datatype: number | string | undefined = undefined;
         if (item.datatype && item.datatype === 'number') {
@@ -144,45 +168,47 @@ export class OracleDatabase implements INodeType {
           datatype = oracledb.STRING;
         }
 
+        executeManyOptions.bindDefs.push({
+          ...defaultExecuteManyOptions,
+          type: datatype,
+        });
+
         if (!item.parseInStatement) {
           //normal process.
-          result[item.name] = { type: datatype, val: item.datatype && item.datatype === 'number' ? Number(item.value) : String(item.value) };
-          return result;
-        } else {
-          //in this else block, we make it possible to use a parameter for an IN statement
-
-          const valList = item.value.toString().split(',');
-          let generatedSqlString = '(';
-          const crypto = require('crypto');
-          for (let i = 0; i < valList.length; i++) {
-            //generate unique parameter names for each item in list
-            const uniqueId: String = crypto.randomUUID().replaceAll('-', '_'); //dashes don't work in parameter names.
-            const newParamName = item.name + uniqueId;
-
-            //add new param to param list
-            result[newParamName] = { type: datatype, val: item.datatype && item.datatype === 'number' ? Number(valList[i]) : String(valList[i]) };
-
-            //create sql sting for list with new param names
-            generatedSqlString += `:${newParamName},`
-          }
-
-          generatedSqlString = generatedSqlString.slice(0, -1) + ')'; //replace trailing comma with closing parenthesis.
-
-          //replace all occurrences of original parameter name with new generated sql
-          query = query.replaceAll(":" + item.name, generatedSqlString);
-
-          return result;
+          bindParameters.push({
+            [item.name]: { type: datatype, val: item.datatype && item.datatype === 'number' ? Number(item.value) : String(item.value) }
+          });
+          continue;
         }
-      }, {});
 
-      //execute query
-      const result = await connection.execute(
-        query, 
-        bindParameters, 
-        {
-          outFormat: oracledb.OUT_FORMAT_OBJECT,
-          autoCommit: true,
-        },
+        //we make it possible to use a parameter for an IN statement
+        const valList = item.value.toString().split(',');
+        let generatedSqlString = '(';
+        const crypto = require('crypto');
+        for (let i = 0; i < valList.length; i++) {
+          //generate unique parameter names for each item in list
+          const uniqueId: String = crypto.randomUUID().replaceAll('-', '_'); //dashes don't work in parameter names.
+          const newParamName = item.name + uniqueId;
+
+          //add new param to param list
+          bindParameters.push({
+            [newParamName]: { type: datatype, val: item.datatype && item.datatype === 'number' ? Number(valList[i]) : String(valList[i]) }
+          });
+
+          //create sql sting for list with new param names
+          generatedSqlString += `:${newParamName},`
+        }
+
+        generatedSqlString = generatedSqlString.slice(0, -1) + ')'; //replace trailing comma with closing parenthesis.
+
+        //replace all occurrences of original parameter name with new generated sql
+        query = query.replaceAll(":" + item.name, generatedSqlString);
+      }
+
+      const result = await connection.executeMany(
+        query,
+        bindParameters,
+        executeManyOptions,
       );
 
       returnItems = this.helpers.returnJsonArray(
